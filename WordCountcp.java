5294595447b1c1da5445bfad03387374b15480cd
Yilin <huyilin.ce@gmail.com>
2014-03-20 15:32:17 +0100

    Final version

--- WordCountcp.java
+++ WordCountcp.java
@@ -1,4 +1,4 @@
-package org.myorg;
+package ch.epfl.data.bigdata.hw2;
 
 import java.io.IOException;
 import java.util.*;
@@ -9,19 +9,17 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
 
-
-public class hw2 {
+public class Run {
 	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
 //		private final static IntWritable one = new IntWritable(1);
-		private Text localkey;
-		private Text localvalue;
-		private Text connectvalue=new Text("C"); //Representing these two points are directly connected
+//		private Text localkey;
 		public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+			Text connectvalue=new Text("C"); //Representing these two points are directly connected
 			String line = value.toString();
 			String [] linearray=line.split(",");
-			localvalue=new Text(linearray[0]);
+			Text localvalue=new Text(linearray[0]);
 			for(int i=1;i<linearray.length;i++){
-				localkey=new Text(linearray[0]+","+linearray[i]);
+				Text localkey=new Text(linearray[0]+","+linearray[i]);
 				output.collect(localkey,connectvalue); //key:pair of direct connected nodes,value:"C":connected
 				for(int j=1;j<i;j++){					
 					     	localkey=new Text(linearray[i]+","+linearray[j]);
@@ -30,10 +28,9 @@ public class hw2 {
 			}
 		}
 	}
-	
+
 	public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
-		private Text relationvalue;
-		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+			public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
 //			int sum = 0;
 			ArrayList<String> valueList= new ArrayList<String>();
 			while (values.hasNext()) {
@@ -45,42 +42,38 @@ public class hw2 {
 		}
 	}
 
-	public static class Map1 extends MapReduceBase implements Mapper<LongWritable, Text, Text, LongWritable> {
-		private Text localkey;
-		private LongWritable localvalue;
-		public void map(LongWritable key, Text value, OutputCollector<Text, LongWritable> output, Reporter reporter) throws IOException {
+	public static class Map1 extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+		public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
 			String line= value.toString();
 			String[] linearray=line.split(",|\\s+");
-			localkey=new Text("T");
-			localvalue=new LongWritable(Integer.parseInt(linearray[2]));
+			Text localkey=new Text("T:");
+			IntWritable localvalue=new IntWritable(Integer.parseInt(linearray[2]));
 			output.collect(localkey,localvalue);
 		}
 	}
-	
-	public static class Reduce1 extends MapReduceBase implements Reducer<Text, LongWritable, Text, LongWritable> {
-		private Text outputsymbol;
-		private Text localkey=new Text("NumberofTriangles:");
-		public void reduce(Text key, Iterator<LongWritable> values, OutputCollector<Text, LongWritable> output, Reporter reporter) throws IOException {
-			long sum = 0;
+
+	public static class Reduce1 extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+			int sum = 0;
 			while (values.hasNext()) {
 				sum += values.next().get();
 			}
 			sum=sum/3;
-			output.collect(localkey,new LongWritable(sum));
+			output.collect(key,new IntWritable(sum));
 		}
 	}
-	
+
+
 	public static void main(String[] args) throws Exception {
 		String [] pathtemparray=args[1].split("/"); 
-		StringBuilder pathtempstr=new StringBuilder();
+		StringBuilder pathtempstr=new StringBuilder();		
 		for(int i=0;i<pathtemparray.length-1;i++){
 			 pathtempstr.append(pathtemparray[i]+"/");
 		}
-		
 		pathtempstr.append("temp");
 		String pathtemp=pathtempstr.toString();    //Specify the path to store the result of first reduce function
-		
-		JobConf conf = new JobConf(hw2.class);
+
+		JobConf conf = new JobConf(Run.class);
 		conf.setJobName("TriangleFindPhase1");
 		conf.setOutputKeyClass(Text.class);
 		conf.setOutputValueClass(Text.class);
@@ -89,7 +82,7 @@ public class hw2 {
 		conf.setReducerClass(Reduce.class);
 		conf.setNumMapTasks(80);
 		conf.setNumReduceTasks(80);
-
+ 	
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
  	
@@ -98,20 +91,18 @@ public class hw2 {
 		JobClient.runJob(conf);
 
 //		The second mapreduce function
-		JobConf confnext=new JobConf(hw2.class);
-//		confnext.setNumMapTasks(1);
-//		confnext.setNumReduceTasks(1);
+		JobConf confnext=new JobConf(Run.class);
 		confnext.setJobName("TriangleFindPhase2");
 		confnext.setOutputKeyClass(Text.class);
-		confnext.setOutputValueClass(LongWritable.class);
+		confnext.setOutputValueClass(IntWritable.class);
 
 		confnext.setMapperClass(Map1.class);
 		confnext.setReducerClass(Reduce1.class);
-		
+ 	
 		confnext.setInputFormat(TextInputFormat.class);
 		confnext.setOutputFormat(TextOutputFormat.class);
 
-		FileInputFormat.setInputPaths(confnext, new Path(pathtemp,"part-00000")); //read data from the temp(Output file of the first mapreduce)
+		FileInputFormat.setInputPaths(confnext, new Path(pathtemp)); //read data from the temp(Output file of the first mapreduce)
 		FileOutputFormat.setOutputPath(confnext, new Path(args[1]));
 		JobClient.runJob(confnext);
 	}
